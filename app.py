31a5c6791fc66e93c9242329012e8a83f16f3150
Kyubyung Chae <kyubyungchae@Kyubyungui-MacBookAir.local>
2022-06-02 22:29:35 +0900

    update result.html

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from crypt import methods
 from flask import Flask, render_template, url_for, request
+from driver import Neo4jConnection as nc
+
+neo4j_uri = "bolt://localhost:7687"
+neo4j_user = "neo4j"
+neo4j_pwd = "0097"
+
 app = Flask(__name__, static_url_path="/templates/")
 # app.config['SERVER_NAME'] = 'flask.dev'
 
@@ -12,7 +18,21 @@ def get_result():
     if request.method == 'POST':
         result = request.form
 
-    return render_template('result.html', result= result)
+    if result is None:
+        return "404 Error!"
+    
+    conn = nc(uri=neo4j_uri, user=neo4j_user, pwd=neo4j_pwd)
+
+    theme = result['theme']
+    city_id_from = result['from']
+    city_id_to = result['to']
+    depart_time  = result['depart_time'] 
+    days = result['days']
+
+    paths = nc.getPaths(conn, city_id_from, city_id_to, depart_time, days)
+    print(paths[0])
+
+    return render_template('result.html', result= result, all_paths={'paths' : paths})
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug = True)    
--- driver.py
+++ driver.py
@@ -30,6 +30,30 @@ class Neo4jConnection:
                 session.close()
         return response
 
+    def getPaths(conn, city_id_from, city_id_to, depart_time, days):
+
+        new_days = "2" if days == "2" else "4"
+    
+        query_string = '''
+            MATCH p=(:city{city_id:"%s"})-[rels:goes*1..%s]->(:city{city_id:"%s"})
+            WHERE rels[0].depart_time < time('%s')
+            WITH nodes(p) as cities, p
+            RETURN DISTINCT cities
+            '''%(city_id_from, new_days, city_id_to, depart_time)
+        q = conn.query(query_string, db='traveldb')
+        result=list() # 바깥 리스트
+        for _ in q:
+            tmp_result=list() # 안쪽 리스트
+            for j in range(len(dict(_)['cities'])):
+                tmp_result.append((dict(dict(_)['cities'][j])['city_nm'],
+                                dict(dict(_)['cities'][j])['city_id'],
+                                dict(dict(_)['cities'][j])['latitude'],
+                                dict(dict(_)['cities'][j])['longitude'] )) # 요소별 삽입 (컬럼명 주의)
+            result.append(tmp_result) # path list를 바깥 리스트에 삽입
+
+        return result 
+
+
 if __name__ == "__main__":
     conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="0097")
     city_id_from = "C01"
@@ -53,9 +77,10 @@ if __name__ == "__main__":
                                dict(dict(_)['cities'][j])['latitude'],
                                dict(dict(_)['cities'][j])['longitude'] )) # 요소별 삽입 (컬럼명 주의)
         result.append(tmp_result) # path list를 바깥 리스트에 삽입
-        print(result)
-    conn.close()
+    
+    print(result[0])
     
     conn.close()
+
 
     
